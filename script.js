8cfc0cddaef18be54350782331bd400c0e831754
Ezzol <18054161@student.hhs.nl>
2021-04-16 08:44:44 +0200

    working nav!

--- script.js
+++ script.js
@@ -17,12 +17,15 @@ const nav = document.getElementById('stickyNav');
 
     function fixNav() {
       if (window.scrollY >= topOfNav) {
-        document.body.style.paddingTop = nav.offsetHeight + 'px';
+        // document.body.style.paddingTop = nav.offsetHeight + 'px';
         document.body.classList.add('fixed-nav');
       } else {
         document.body.classList.remove('fixed-nav');
-        document.body.style.paddingTop = 0;
+        // document.body.style.paddingTop = 0;
       }
     }
 
     window.addEventListener('scroll', fixNav);
+
+
+    
